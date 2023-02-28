229b0fed78b79c6bb7d382608a8581179d37eea3
semperprimum <bkeem0266+github@gmail.com>
2023-02-28 15:14:45 +0600

    third commit

--- pdo.php
+++ pdo.php
@@ -17,6 +17,13 @@ if (isset($_POST["category_name"])) {
     unset($_POST["category_name"]);
 }
 
+if (isset($_POST["delete_id"])) {
+    $id = $_POST["delete_id"];
+    $sql = "DELETE FROM categories WHERE id=?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +34,16 @@ if (isset($_POST["category_name"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro-v6@44659d9/css/all.min.css" rel="stylesheet" type="text/css" />
     <title>Document</title>
+    <style>
+        .btn__delete {
+            background: none;
+            border: none;
+            color: red;
+            font-size: 1.1em;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-3">
@@ -51,6 +67,7 @@ if (isset($_POST["category_name"])) {
                     <th>Category Name</th>
                     <th>Parent Category ID</th>
                     <th>Parent Category Name</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -73,6 +90,10 @@ if (isset($_POST["category_name"])) {
                         } else echo "NULL";
                         ?>
                     </td>
+                    <td><form method="post">
+                        <input type="hidden" name="delete_id" value="<?= $row['id'] ?>">
+                        <button class="btn__delete"><i class="fa-regular fa-trash-slash"></i></button>
+                    </form></td>
                 </tr>
                 <?php endwhile ?>
             </tbody>
